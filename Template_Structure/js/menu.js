e7319e01f05a265bb3f94744790dc9f3c30cf75b
ClaireChen3758 <CONGCHEN1024@GMAIL.COM>
2016-06-29 08:59:06 -0700

    fix white space at homepage and upcoming page fix responsive problem at menu.js hamburger icon appear at breakpoint(732px)

--- Template_Structure/js/menu.js
+++ Template_Structure/js/menu.js
@@ -1,35 +1,39 @@
 $(document).ready( function() {
-	
-	var width = $(window).width();
 
-
-	if (width < 400) {
-		$(".nav-toggle").removeClass("hidden");
-		$(".nav-toggle").addClass("shown"); // Adding a class to show our toggle
-		$(".nav-mobile").removeClass("hidden");
-		$(".nav-mobile").addClass("shown"); 
-        $(".logo").removeClass("shown");
-		$(".logo").addClass("hidden"); 
-        
-
-		$("nav").hide();	
-
-		console.log("hidden");	// Setting nav to hide using jQuery/Zepto's method
-
-	}else {
-
-		$(".nav-toggle").removeClass("shown");
-		$(".nav-toggle").addClass("hidden"); 
-		$(".nav-mobile").removeClass("shown");
-		$(".nav-mobile").addClass("hidden"); 
-        $(".logo").removeClass("hidden");
-		$(".logo").addClass("shown"); 
-
+	$(window).resize( function() {
 		
-		$("nav").show();
+		var width = $(window).width();
 
 
-	}
+		if (width < 732) {
+			$(".nav-toggle").removeClass("hidden");
+			$(".nav-toggle").addClass("shown"); // Adding a class to show our toggle
+			$(".nav-mobile").removeClass("hidden");
+			$(".nav-mobile").addClass("shown"); 
+	        $(".logo").removeClass("shown");
+			$(".logo").addClass("hidden"); 
+	        
+
+			$("nav").hide();	
+
+			console.log("hidden");	// Setting nav to hide using jQuery/Zepto's method
+
+		}else {
+
+			$(".nav-toggle").removeClass("shown");
+			$(".nav-toggle").addClass("hidden"); 
+			$(".nav-mobile").removeClass("shown");
+			$(".nav-mobile").addClass("hidden"); 
+	        $(".logo").removeClass("hidden");
+			$(".logo").addClass("shown"); 
+
+			
+			$("nav").show();
+
+
+		}
+
+	}); // end of .resize()
 
 	/*
 		Here we are running a function when #navToggle is clicked
