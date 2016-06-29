0182bd1479dbecae858e6c97f4bd1405924204bb
ClaireChen3758 <CONGCHEN1024@GMAIL.COM>
2016-06-28 18:04:26 -0700

    update function for click buttom update purchase js

--- Template_Structure/js/menu.js
+++ Template_Structure/js/menu.js
@@ -11,17 +11,30 @@ $(document).ready( function() {
         $(".logo").removeClass("shown");
 		$(".logo").addClass("hidden"); 
         
+<<<<<<< HEAD
 		$("nav").hide();		// Setting nav to hide using jQuery/Zepto's method
+=======
+		$("nav").hide();	
+
+		console.log("hidden");	// Setting nav to hide using jQuery/Zepto's method
+>>>>>>> e60d575... update function for click buttom
 	}else {
 
-		$(".nav-toggle").removeClass("shown"); //not working
-		$(".nav-toggle").addClass("hidden"); // Adding a class to show our toggle
+		$(".nav-toggle").removeClass("shown");
+		$(".nav-toggle").addClass("hidden"); 
 		$(".nav-mobile").removeClass("shown");
 		$(".nav-mobile").addClass("hidden"); 
         $(".logo").removeClass("hidden");
 		$(".logo").addClass("shown"); 
+<<<<<<< HEAD
         
 		$("nav").show();		// Setting nav to show using jQuery/Zepto's method
+=======
+		
+		$("nav").show();
+
+			// Setting nav to show using jQuery/Zepto's method
+>>>>>>> e60d575... update function for click buttom
 	}
 
 	/*
